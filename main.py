bab0c2196c505d67ae2d8e4c18af7712b7825019
Haider Al-Tahan <haideraltahan@gmail.com>
2020-08-02 07:14:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         def block(in_feat, out_feat, normalize=True):
-            layers = [Linear(in_feat, out_feat)]
+            layers = [nn.Linear(in_feat, out_feat)]
             if normalize:
                 layers.append(nn.BatchNorm1d(out_feat, 0.8))
             layers.append(nn.LeakyReLU(0.2, inplace=True))
